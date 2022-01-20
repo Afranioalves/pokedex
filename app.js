a66074ab663d2d000b89edd9b744490c3811e80f
Carlos Daniel <carlosdandeveloper@gmail.com>
2022-01-20 16:49:41 -0300

    Alterações nos estilos

--- app.js
+++ app.js
@@ -11,89 +11,89 @@ const generateHTML = (pokemons) =>
   pokemons.reduce((acc, { id, name, types, stats }) => {
     const elementTypes = types.map((typeInfo) => typeInfo.type.name);
     acc += `  
-    <li class="pokedex__item ${elementTypes[0]}">
-      <h2 class="pokemon__name">${id}. ${name}</h2>
-      <div class="pokemon__types">
-        ${
-          elementTypes.length > 1
-            ? `<p>${elementTypes[0]}</p> <p>${elementTypes[1]}</p>`
-            : `<p>${elementTypes[0]}</p>`
-        }
-      </div>
-      <img class="pokeball" src="./img/pokeball.svg" alt="Pokeball"/>
-      <img class="pokemon__image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png" alt="${name}">
-      <div class="pokemon__stats">
-        <h3>Base Stats</h3>
-        <ul class="stats__list">
-          <li>
-            <span>HP</span>
-            <span>${stats[0].base_stat}</span>
-            <div class="bar">
+      <li class="pokedex__item ${elementTypes[0]}">
+        <h2 class="pokemon__name">${id}. ${name}</h2>
+        <div class="pokemon__types">
+          ${
+            elementTypes.length > 1
+              ? `<p>${elementTypes[0]}</p> <p>${elementTypes[1]}</p>`
+              : `<p>${elementTypes[0]}</p>`
+          }
+        </div>
+        <img class="pokeball" src="./img/pokeball.svg" alt="Pokeball"/>
+        <img class="pokemon__image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png" alt="${name}">
+        <div class="pokemon__stats">
+          <h3>Base Stats</h3>
+          <ul class="stats__list">
+            <li>
+              <span>HP</span>
+              <span>${stats[0].base_stat}</span>
+              <div class="bar">
+                <div class="bar__progress" style="${
+                  stats[0].base_stat >= 50
+                    ? `background: #49D0B0; width: ${stats[0].base_stat}%`
+                    : `width: ${stats[0].base_stat}%`
+                }"></div>
+              </div>
+            </li>
+            <li>
+              <span>Attack</span>
+              <span>${stats[1].base_stat}</span>
+              <div class="bar">
               <div class="bar__progress" style="${
-                stats[0].base_stat >= 50
-                  ? `background: #49D0B0; width: ${stats[0].base_stat}%`
-                  : `width: ${stats[0].base_stat}%`
+                stats[1].base_stat >= 50
+                  ? `background: #49D0B0; width: ${stats[1].base_stat}%`
+                  : `width: ${stats[1].base_stat}%`
               }"></div>
-            </div>
-          </li>
-          <li>
-            <span>Attack</span>
-            <span>${stats[1].base_stat}</span>
-            <div class="bar">
-            <div class="bar__progress" style="${
-              stats[1].base_stat >= 50
-                ? `background: #49D0B0; width: ${stats[1].base_stat}%`
-                : `width: ${stats[1].base_stat}%`
-            }"></div>
-            </div>
-          </li>
-          <li>
-            <span>Defense</span>
-            <span>${stats[2].base_stat}</span>
-            <div class="bar">
-            <div class="bar__progress" style="${
-              stats[2].base_stat >= 50
-                ? `background: #49D0B0; width: ${stats[2].base_stat}%`
-                : `width: ${stats[2].base_stat}%`
-            }"></div>
-            </div>
-          </li>
-          <li>
-            <span>Sp.Atk</span>
-            <span>${stats[3].base_stat}</span>
-            <div class="bar">
-            <div class="bar__progress" style="${
-              stats[3].base_stat >= 50
-                ? `background: #49D0B0; width: ${stats[3].base_stat}%`
-                : `width: ${stats[3].base_stat}%`
-            }"></div>
-            </div>
-          </li>
-          <li>
-            <span>Sp.Def</span>
-            <span>${stats[4].base_stat}</span>
-            <div class="bar">
-            <div class="bar__progress" style="${
-              stats[4].base_stat >= 50
-                ? `background: #49D0B0; width: ${stats[4].base_stat}%`
-                : `width: ${stats[4].base_stat}%`
-            }"></div>
-            </div>
-          </li>
-          <li>
-            <span>Speed</span>
-            <span>${stats[5].base_stat}</span>
-            <div class="bar">
-            <div class="bar__progress" style="${
-              stats[5].base_stat >= 50
-                ? `background: #49D0B0; width: ${stats[5].base_stat}%`
-                : `width: ${stats[5].base_stat}%`
-            }"></div>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </li>`;
+              </div>
+            </li>
+            <li>
+              <span>Defense</span>
+              <span>${stats[2].base_stat}</span>
+              <div class="bar">
+              <div class="bar__progress" style="${
+                stats[2].base_stat >= 50
+                  ? `background: #49D0B0; width: ${stats[2].base_stat}%`
+                  : `width: ${stats[2].base_stat}%`
+              }"></div>
+              </div>
+            </li>
+            <li>
+              <span>Sp.Atk</span>
+              <span>${stats[3].base_stat}</span>
+              <div class="bar">
+              <div class="bar__progress" style="${
+                stats[3].base_stat >= 50
+                  ? `background: #49D0B0; width: ${stats[3].base_stat}%`
+                  : `width: ${stats[3].base_stat}%`
+              }"></div>
+              </div>
+            </li>
+            <li>
+              <span>Sp.Def</span>
+              <span>${stats[4].base_stat}</span>
+              <div class="bar">
+              <div class="bar__progress" style="${
+                stats[4].base_stat >= 50
+                  ? `background: #49D0B0; width: ${stats[4].base_stat}%`
+                  : `width: ${stats[4].base_stat}%`
+              }"></div>
+              </div>
+            </li>
+            <li>
+              <span>Speed</span>
+              <span>${stats[5].base_stat}</span>
+              <div class="bar">
+              <div class="bar__progress" style="${
+                stats[5].base_stat >= 50
+                  ? `background: #49D0B0; width: ${stats[5].base_stat}%`
+                  : `width: ${stats[5].base_stat}%`
+              }"></div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </li>`;
     return acc;
   }, "");
 
